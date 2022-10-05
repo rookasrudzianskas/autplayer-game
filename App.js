1270c02502d8a42da3c9ddb84a1f60959347b09f
Rokas <rookas.rudzenskas@gmail.com>
2022-10-05 08:35:07 +0300

    Rokas

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import bg from './assets/bg.jpeg';
 export default function App() {
   return (
     <View className="bg-[#242D34]">
-      <ImageBackground source={bg} style={{width: '100%', height: '100%'}} className="items-center justify-center" resizeMode={'contain'} >
-        <View className="w-[90px] h-[90px] rounded-full bg-red-500"/>
+      <ImageBackground source={bg} style={{width: '100%', height: '100%', paddingTop: 20}} className="items-center justify-center" resizeMode={'contain'} >
+        <View className="w-[75px] h-[75px] rounded-full bg-white items-center justify-center">
+          <View className="w-[55px] h-[55px] bg-[#242D34] rounded-full"/>
+        </View>
       </ImageBackground>
     </View>
   );
