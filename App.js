88d271eae48d19ae22b95cb27d9441e6b0d81c51
Rokas <rookas.rudzenskas@gmail.com>
2022-10-05 08:29:02 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import bg from './assets/bg.jpeg';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <ImageBackground source={bg} style={{width: '100%', height: '100%'}} resizeMode={'cover'} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
