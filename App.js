3111ea9cab56e7d6abce2b2a5f744b3960fd61f6
Rokas <rookas.rudzenskas@gmail.com>
2022-10-05 08:38:14 +0300

    Roka

--- App.js
+++ App.js
@@ -6,8 +6,21 @@ export default function App() {
   return (
     <View className="bg-[#242D34]">
       <ImageBackground source={bg} style={{width: '100%', height: '100%', paddingTop: 20}} className="items-center justify-center" resizeMode={'contain'} >
-        <View className="w-[75px] h-[75px] rounded-full bg-white items-center justify-center">
+        <View className="w-[75px] h-[75px] mb-10 rounded-full bg-white items-center justify-center">
           <View className="w-[55px] h-[55px] bg-[#242D34] rounded-full"/>
+        </View>
+
+        <View className="relative">
+          <View className="absolute w-[10px] h-[100px] bg-white" style={{transform: [
+              {
+                rotate: '45deg',
+              }
+            ]}} />
+          <View className="absolute w-[10px] h-[100px] bg-white" style={{transform: [
+              {
+                rotate: '-45deg',
+              }
+            ]}} />
         </View>
       </ImageBackground>
     </View>
