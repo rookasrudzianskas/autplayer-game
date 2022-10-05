5a28ff038a9ae135d7f66433dbf405cec524a99e
Rokas <rookas.rudzenskas@gmail.com>
2022-10-05 09:42:43 +0300

    Rokas

--- App.js
+++ App.js
@@ -4,28 +4,58 @@ import bg from './assets/bg.jpeg';
 
 export default function App() {
   return (
-    <View className="bg-[#242D34]">
-      <ImageBackground source={bg} style={{width: '100%', height: '100%', paddingTop: 20}} className="items-center justify-center" resizeMode={'contain'} >
-        <View className="border border-white" style={{width: '80%', aspectRatio: 1}}>
-          <View className="w-[75px] h-[75px] mb-10 rounded-full bg-white items-center justify-center">
-            <View className="w-[55px] h-[55px] bg-[#242D34] rounded-full"/>
-          </View>
+    <View style={styles.container} className="bg-[#242D34]">
+      <ImageBackground source={bg} style={styles.bg} resizeMode={'contain'} >
+        <View style={styles.map}>
+            <View style={styles.circle} />
 
-          <View className="relative">
-            <View className="absolute w-[10px] h-[100px] bg-white" style={{transform: [
-                {
-                  rotate: '45deg',
-                }
-              ]}} />
-            <View className="absolute w-[10px] h-[100px] bg-white" style={{transform: [
-                {
-                  rotate: '-45deg',
-                }
-              ]}} />
-          </View>
+            <View>
+              <View style={styles.crossLina} />
+              <View style={[styles.crossLina, styles.crossLineReversed]} />
+            </View>
         </View>
       </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#242D34',
+  },
+  bg: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 15,
+  },
+  map: {
+
+  },
+  circle: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    borderWidth: 10,
+    borderColor: '#fff',
+  },
+  innerCircle: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#242D34',
+    borderRadius: 50,
+  },
+  crossLine: {
+    position: 'absolute',
+    width: 10,
+    height: 70,
+  }
+});
 
